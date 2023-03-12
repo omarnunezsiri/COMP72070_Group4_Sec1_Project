6719c8424b6ef29be88cf94e4aa97aafd381985b
Omar Nunez <omarnunezsiri@gmail.com>
2023-03-12 16:44:37 -0400

    Implement password get/set to fix #1 and #3

--- Server/Account.cs
+++ Server/Account.cs
@@ -11,8 +11,8 @@ namespace Server
         private string _password;
         private string _username;
 
-        public string getPassword() { return null;  }
-        public void setPassword(string pw) { }
+        public string getPassword() { return _password;  }
+        public void setPassword(string pw) { _password = pw;  }
 
         public string getUsername() { return null; }
         public void setUsername(string username) { }
