b54fa541737b3ffd1f224565bccc8594ca1f4190
SebOuellette <seb_ouellette@honeybeeks.net>
2023-03-13 10:53:10 -0400

    Created the packet test methods
    There is no content within them, they're just the shell.

--- Shared.Tests/SharedTests.cs
+++ Shared.Tests/SharedTests.cs
@@ -62,4 +62,105 @@ namespace Shared.Tests
             Assert.AreEqual(_USERNAME, actual);
         }
     }
+
+    [TestClass]
+    public class PacketTests
+    {
+        [TestMethod]
+        public void PACKETUNIT001_SerializeHeader_CorrectBytesAllocated()
+        {
+
+        }
+
+        [TestMethod]
+        public void PACKETUNIT002_SerializeDownloadBody_CorrectBytesAllocated()
+        {
+
+        }
+
+        [TestMethod]
+        public void PACKETUNIT003_SerializeMediaBody_CorrectBytesAllocated()
+        {
+
+        }
+
+        [TestMethod]
+        public void PACKETUNIT004_SerializeSyncBody_CorrectBytesAllocated()
+        {
+
+
+        }
+
+        [TestMethod]
+        public void PACKETUNIT005_SerializeAccountBody_CorrectBytesAllocated()
+        {
+
+        }
+
+        [TestMethod]
+        public void PACKETUNIT006_SerializeSearchBody_CorrectBytesAllocated()
+        {
+
+        }
+
+        [TestMethod]
+        public void PACKETUNIT007_SerializeSongData_CorrectBytesAllocated()
+        {
+
+        }
+
+        [TestMethod]
+        public void PACKETUNIT008_SerializeImageData_CorrectBytesAllocated()
+        {
+
+        }
+
+        [TestMethod]
+        public void PACKETUNIT009_DeserializeHeader_CorrectMembersAssigned()
+        {
+
+        }
+
+        [TestMethod]
+        public void PACKETUNIT010_DeserializeDownloadBody_CorrectMembersAssigned()
+        {
+
+        }
+
+        [TestMethod]
+        public void PACKETUNIT011_DeserializeMediaBody_CorrectMembersAssigned()
+        {
+
+        }
+
+        [TestMethod]
+        public void PACKETUNIT012_DeserializeSyncBody_CorrectMembersAssigned()
+        {
+
+        }
+
+        [TestMethod]
+        public void PACKETUNIT013_DeserializeAccountBody_CorrectMembersAssigned()
+        {
+
+        }
+
+        [TestMethod]
+        public void PACKETUNIT014_DeserializeSearchBody_CorrectMembersAssigned()
+        {
+
+        }
+
+        [TestMethod]
+        public void PACKETUNIT015_DeserializeSongData_CorrectMembersAssigned()
+        {
+
+        }
+
+        [TestMethod]
+        public void PACKETUNIT016_DeserializeImageData_CorrectMembersAssigned()
+        {
+
+        }
+    }
 }
