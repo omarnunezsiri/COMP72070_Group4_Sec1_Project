3069283d9efddbb8571a8a51febc9c7faad1bbcf
Ajdj100 <ajairplane@gmail.com>
2023-03-23 22:46:07 -0400

    Some basic client audio playing stuff. For some reason the test project wouldnt connect. Abandoning for now in favor of working on server.

---
+++ ClientLibrary/Class1.cs
@@ -0,0 +1,27 @@
+﻿using NAudio.Wave;
+
+namespace ClientLibrary
+{
+    public class AudioHandler
+    {
+        public void playSong(String songFileName)
+        {
+            var song = new AudioFileReader(songFileName);
+            var outputDevice = new WaveOutEvent();
+
+            outputDevice.Init(song);
+            outputDevice.Play();
+
+            while (outputDevice.PlaybackState == PlaybackState.Playing) //this is dumb lol
+            {
+                Thread.Sleep(1000);
+            }
+        }
+
+        public bool cleanupTempFile()
+        {
+            bool success = false;
+
+        }
+    }
+}
---
+++ ClientTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+
+namespace ClientTests
+{
+    [TestClass]
+    public class AUDIO_UNIT_TESTS
+    {
+        [TestMethod]
+        public void AUDIOUNIT001_PlayAudio()
+        {
+            playSong("temp.mp3");
+        }
+
+        public void AUDIOUNIT002_PlayAudioFromMemory() 
+        {
+
+        }
+    }
+}
