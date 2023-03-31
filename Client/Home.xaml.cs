391d3860c65eb75bd7fff2c255579fef02433cc8
Ajdj100 <ajairplane@gmail.com>
2023-03-31 08:41:19 -0400

    Willed search into existence. - Major visual issues (scaling is wack) - All data comms parts need to be updated - Album images are turned off (I dont know where they are getting stored on the client) - Need to add error handling in case image cannot be found - For gods sake Alistair change the search background colour - Remove the testing array and song class

--- Client/Home.xaml.cs
+++ Client/Home.xaml.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.VisualBasic;
 using NAudio.Utils;
 using NAudio.Wave;
+using Server;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 
 namespace Client
 {
@@ -22,9 +27,37 @@ namespace Client
     /// </summary>
     public partial class Home : Window
     {
+        //list of song names to pretend to select from as if it was a database of songs                 DELETE THIS
+        public List<song> fakeResults { get; set; } = new List<song>
+        {
+            new song{ Name="One More Light", Artist = "Linkin Park"},
+            new song{Name="Anti-Hero", Artist="Taylor Swift"},
+            new song{Name="Hello", Artist="Adele"},
+            new song{Name="Golden Hour",  Artist="JVKE"}
+        };
+
         BackgroundWorker BackgroundWorker { get; set; }
         State playingState = new();
         WaveOutEvent outputDevice = new();
+
+        //search stuff
+        //this list "searchResults" should be populated with the search results from the server
+        public List<song> searchResults { get; set; } = new List<song> { };
+
+        List<Grid> buttonList = new List<Grid>();   //list for storing search result buttons
+
+        //next up is a bunch of trash that defines the size and where the buttons appear
+
+
+        int currentY = 0; //Y location of the current button
+        int Xloc = 0; //X location to put every button
+
+        //button dimensions
+        const int WIDTH = 400;
+        const int HEIGHT = 60;
+
+        //is the search view currently showing
+        bool searchActive = false;
 
         public Home()
         {
@@ -207,6 +240,165 @@ namespace Client
             Login newWindow = new Login();
             newWindow.Show();
             Close();
+        }
+
+        //search stuff
+        /// <summary>
+        /// CURRENTLY UNUSED Clears search results
+        /// </summary>
+        public void clearSearch()
+        {
+            if (searchActive)
+            {
+                foreach (Grid button in buttonList)
+                {
+                    MainCanvas.Children.Remove(button);
+                }
+                searchActive = false;
+                currentY = 64;
+
+                searchResults.Clear();
+            }
+        }
+
+        /// <summary>
+        /// stubbed search function, will pick 5 randoms names from 'fakeResults' NEED TO REPLACE WITH MAKING SEARCH REQUEST
+        /// </summary>
+        /// <param name="resultList"></param>
+        public void search(List<song> resultList)
+        {
+            Random r = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                int index = r.Next(4);
+                resultList.Add(fakeResults[index]);
+            }
+        }
+
+        /// <summary>
+        /// creates a new button for the user to see and click on (THIS COULD PROBABLY BE BROKEN DOWN INTO MORE OR SOMETING
+        /// </summary>
+        private void MakeButton()
+        {
+            buttonList.Clear();
+            search(searchResults);      //this makes the search results. Change this to be passed in
+
+            for (int i = 0; i < searchResults.Count; i++)
+            {
+                //create the grid container for the search item
+                Grid newGrid = new Grid();
+                newGrid.Style = (Style)FindResource("ResultGrid");
+                newGrid.Name = "item" + i;
+
+                //oh god here we go. Create all the rows and columns
+                ColumnDefinition col1 = new ColumnDefinition();
+                col1.Width = new GridLength(60);
+                ColumnDefinition col2 = new ColumnDefinition();
+                RowDefinition row1 = new RowDefinition();
+                RowDefinition row2 = new RowDefinition();
+                newGrid.ColumnDefinitions.Add(col1);
+                newGrid.ColumnDefinitions.Add(col2);
+                newGrid.RowDefinitions.Add(row1);
+                newGrid.RowDefinitions.Add(row2);
+                newGrid.MouseDown += SelectSong;
+
+
+                ////create the album image
+                //System.Windows.Controls.Image albumCover = new Image();
+                //albumCover.Style = (Style)FindResource("AlbumImage");
+
+                ////jesus christ giant bitmap thing that will probably be replaced anyways
+                //FileStream fs = File.Open("Tmp/" + searchResults[i].Name + ".png", FileMode.Open);
+                //System.Drawing.Bitmap dImg = new System.Drawing.Bitmap(fs);
+                //MemoryStream ms = new MemoryStream();
+                //dImg.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                //System.Windows.Media.Imaging.BitmapImage bImg = new System.Windows.Media.Imaging.BitmapImage();
+                //bImg.BeginInit();
+                //bImg.StreamSource = new MemoryStream(ms.ToArray());
+                //bImg.EndInit();
+
+                //albumCover.Source = bImg;
+
+                //fs.Close();
+
+
+                //create the song name text
+                TextBlock songName = new TextBlock();
+                songName.Style = (Style)FindResource("SongName");
+                songName.Text = searchResults[i].Name;
+
+                //Create artist text block
+                TextBlock artistName = new TextBlock();
+                artistName.Style = (Style)FindResource("ArtistName");
+                artistName.Text = searchResults[i].Artist;
+
+                //add all elements to grid
+                //newGrid.Children.Add(albumCover);             //TEMP COMMENTING OUT, NEED TO FIGURE OUT WHERE THESE IMAGES ARE GETTING PUT
+                newGrid.Children.Add(songName);
+                newGrid.Children.Add(artistName);
+
+                buttonList.Add(newGrid);
+                //Button newBtn = new Button();
+                //newBtn.Content = searchResults[i];
+                //newBtn.Name = "button" + i.ToString();
+                //newBtn.Width = WIDTH;
+                //newBtn.Height = HEIGHT;
+                //newBtn.Click += NewBtn_Click;
+                //buttonList.Add(newBtn);
+            }
+        }
+
+        /// <summary>
+        /// Selects song to play                NEED TO 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SelectSong(object sender, MouseButtonEventArgs e)
+        {
+            Grid clickedGrid = (Grid)sender;
+            string clickedItem = clickedGrid.Name.Remove(0, 4);
+            Debug.WriteLine(clickedItem);
+            //clearSearch();
+            int i = Int32.Parse(clickedItem);
+
+            
+            Console.WriteLine(searchResults[i].ToString());  //this is supposed to be the request to stream/play the song. Replace with requesting song from server
+        }
+
+        /// <summary>
+        /// when clicking enter on search bar
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (!searchActive)
+                {
+                    MakeButton();
+                    foreach (Grid button in buttonList)
+                    {
+                        Canvas.SetTop(button, currentY);
+                        Canvas.SetLeft(button, Xloc);
+                        currentY += HEIGHT;
+                        MainCanvas.Children.Add(button);    //adds the button to the canvas
+                    }
+                    searchActive = true;
+                }
+            }
+        }
+    }
+
+    public class song
+    {
+        public string Name { get; set; }
+        public string Artist { get; set; }
+
+
+        public override string ToString()
+        {
+            return Name + " by " + Artist;
         }
     }
 }
